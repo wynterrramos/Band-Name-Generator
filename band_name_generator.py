98ded77cdc33fe43abe0443a58e4ac0019aab066
wynterrramos <ramoswynter@gmail.com>
2025-06-07 03:14:03 -0500

    band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,3 +3,4 @@ print("Welcome to the Band Name Generator!") # Greet the user
 city = input("Which city did you grow up in?\n") # Ask for the city the user grew up in
 pet_name = input("What is the name of your pet?\n") # Ask for the user for the name of their pet
 print("Your band name could be: " + city + " " + pet_name) # Combine the inputs to create a band name
+input("Press Enter to exit...") # Keeps the window open after the script finishes 
